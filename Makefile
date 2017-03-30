a39257227299d7602bff54dca6cde2c1dc179053
Matthew Nathan Blanchard <matthew.n.blanchard@maine.edu>
2017-03-30 10:16:40 -0400

    Tweaked file operations in morse.c, added user test program

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ obj-m += morse.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
-
+	$(CC) test.c -o test        
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	rm test
---
+++ test.c
@@ -0,0 +1,13 @@
+/* Matthew Blanchard
+ * ECE 331
+ * Title: test.c
+ * Description: kmorse test program. This program aims to
+ *      test the capabilities of the kmorse kernel driver.
+ */
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+        return 0;
+}
