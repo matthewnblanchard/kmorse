08fac1ba3b61bfa77fc9fff3111784b495799b07
Matthew Nathan Blanchard <matthew.n.blanchard@maine.edu>
2017-04-20 00:05:17 -0400

    regress now accepts mutliple arguments and forks to attempt to run them concurrently, in order to test locking

--- regress.c
+++ regress.c
@@ -1,8 +1,11 @@
 /* Matthew Blanchard
  * ECE 331
- * Title: test.c
- * Description: kmorse test program. This program aims to
- *      test the capabilities of the kmorse kernel driver.
+ * Title: regress.c
+ * Description: bmorse test program. This program aims to test the effectiveness 
+ *      of the bmorse module in tandem with the helper script pl-regress. Regress.c
+ *      accepts multiple string arguments and forks to send each string to the module
+ *      at the same time, in order to test locking. pl-regress sends words to the 
+ *      regress program in sets to exhaustively test locking.
  */
 
 #include<stdio.h>
@@ -15,32 +18,38 @@
 
 int main(int argc, char *argv[])
 {
+        int i = 0;       // Loop index
         int fd = 0;      // File descriptor
         size_t size = 0; // String size
+        pid_t child;     // PID for forking
 
-        printf("Checking argument count ... \n");
-        if (argc != 2) {
-                printf("Error: expecting one argument ... \n");
-                return 1;
+        // Create a child process for each argument
+        for (i = 1; i < argc; i++) {
+                child = fork();           // Create child
+                if (child == 0) {         // If this is the child, exit the loop
+                        break;  
+                } else if (child < 0) {   // If the child was not created, note it and keep going
+                        printf("Warning: failed to create child process for %s argument!\n", argv[i]);
+                }      
         }
 
-        size = strlen(argv[1]); // String size
+        // Child processess write their argument to bmorse
+        if (child == 0) {
+                size = strlen(argv[i]); // String size
        
-        printf("Opening bmorse ... \n"); 
-        fd = open("/dev/morse", O_WRONLY);
-        if (fd <= 0) {
-                printf("Error: failed to open bmorse for writing\n");
-                return 2;
-        }
+                fd = open("/dev/morse", O_WRONLY);
+                if (fd <= 0) {
+                        printf("Error: failed to open bmorse for writing\n");
+                        return 2;
+                }
        
-        printf("Writing %s to bmorse ... \n", argv[1]); 
-        if (size != write(fd, argv[1], size)) {
-                printf("Error: failed to write to bmorse\n");
-                return 3;
-        }
+                if (size != write(fd, argv[i], size)) {
+                        printf("Error: failed to write to bmorse\n");
+                        return 3;
+                }
 
-        printf("Closing bmorse ... \n");
-        close(fd);
-        printf("Exiting ... \n");
+                close(fd);
+                return 0;
+        }
         return 0;
 }
